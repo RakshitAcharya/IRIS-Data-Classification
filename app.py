0ccd7900732602ba992967193c6748ec3ae9e412
Rakshit Acharya <69286061+RakshitAcharya@users.noreply.github.com>
2021-03-07 22:21:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,27 +6,22 @@ import pickle
 lin_model=pickle.load(open('lin_model.pkl','rb'))
 knn_model=pickle.load(open("knn_iris.pkl","rb"))
 
-def classify(num):
-    if num<0.5:
-        return 'Setosa'
-    elif num <1.5:
-        return 'Versicolor'
-    else:
-        return 'Virginica'
-def main():
+
    
-    st.title("Streamlit Tutorial")
-    st.header("                      IRIS CLASSIFICATION")
-    sl=st.slider('Select Sepal Length', 0.0, 10.0)
-    sw=st.slider('Select Sepal Width', 0.0, 10.0)
-    pl=st.slider('Select Petal Length', 0.0, 10.0)
-    pw=st.slider('Select Petal Width', 0.0, 10.0)
-    inputs=[[sl,sw,pl,pw]]
-    if st.button('Classify'):
-        st.success(classify(knn_model.predict(inputs)))
+st.title("Streamlit Tutorial")
+st.header("                      IRIS CLASSIFICATION")
+sl=st.slider('Select Sepal Length', 0.0, 10.0)
+sw=st.slider('Select Sepal Width', 0.0, 10.0)
+pl=st.slider('Select Petal Length', 0.0, 10.0)
+pw=st.slider('Select Petal Width', 0.0, 10.0)
+inputs=[[sl,sw,pl,pw]]
+if st.button('Classify'):
+    num=(knn_model.predict(inputs))
+    if num<0.5:
+        st.success("Setosa")
+    elif num <1.5:
+        st.success('Versicolor')
+    else:
+        st.success('Virginica')
     
    
-
-
-if __name__=='__main__':
-    main()
